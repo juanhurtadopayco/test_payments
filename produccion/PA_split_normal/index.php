4379031854451834547f46bb21e2d81009985962
Juan Pablo <juan.hurtado@payco.co>
2021-05-28 07:17:33 -0500

    split porcentajes

--- produccion/PA_split_normal/index.php
+++ produccion/PA_split_normal/index.php
@@ -2,24 +2,23 @@
 $p_id_invoice=time();
 $p_cust_id_cliente='33487';//Id del comercio propietario de la aplicación
 $p_key='b0a717703d99929c40108f71bdcdbea15efb27cf';//p_key del comercio propietario de la aplicación
-$p_amount='100';
+$p_amount='100000';
 $p_tax="0";
 $p_base="0";
-$p_currency_code='cop';
-$p_description = 'Prueba Split Payment PHP';
+$p_currency_code='COP';
+$p_description = 'Porcentajes Split Payment PHP';
 
 $p_signature= md5($p_cust_id_cliente.'^'.$p_key.'^'.$p_id_invoice.'^'.$p_amount.'^'.$p_currency_code);
 
 $p_split_type='02';// 01 Fijo | 02 Porcentual
 $p_split_merchant_receiver='33487';//Id Comercio dueño del producto o servicio
 $p_split_primary_receiver='33487';//Id del cliente recibidor primario (App, Maketplace, Tienda, etc)
-$p_split_primary_receiver_fee='10';//Comisión del recibidor primario (App,Maketplace,Tienda,etc)
 $p_split_receivers=array();
 //$p_signature_receivers Y $p_split_receivers SOLO ES NECESARIO SI SON MÁS DE UN RECIBIDOR 
 $p_signature_receivers="";
 $p_split_receivers = array(
-    array('id'=>'30085','fee'=>'10'),
-    array('id'=>'69205','fee'=>'20')
+    array('id'=>'30085','fee'=>'50000'),
+    array('id'=>'69205','fee'=>'50000')
 );
 
 foreach($p_split_receivers as $receiver){
@@ -35,20 +34,19 @@ $p_signature_split=md5($p_split_type.'^'.$p_split_merchant_receiver.'^'.$p_split
     <input name="p_key" type="hidden" value="<?php echo $p_key ?>">
     <input name="p_id_invoice" type="hidden" value="<?php  echo $p_id_invoice ?>">
     <input name="p_description" type="hidden" value="<?php  echo $p_description ?>">
-    <input name="p_currency_code" type="hidden" value="cop">
+    <input name="p_currency_code" type="hidden" value="COP">
     <input name="p_amount" id="p_amount" type="hidden" value="<?php echo $p_amount ?>">
     <input name="p_tax" id="p_tax" type="hidden" value="0">
     <input name="p_amount_base" id="p_amount_base" type="hidden" value="0">
-    <input name="p_test_request" type="hidden" value="false">
+    <input name="p_test_request" type="hidden" value="true">
     <input name="p_url_response" type="hidden" value="">
     <input name="p_url_confirmation" type="hidden" value="">
     <input name="p_signature" type="hidden" id="signature"  value="<?php echo $p_signature ?>" />
     <input name="p_split_type" type="hidden" value="<?php echo $p_split_type ?>">
     <input name="p_split_merchant_receiver" type="hidden" value="<?php echo $p_split_merchant_receiver ?>">
     <input name="p_split_primary_receiver" type="hidden" value="<?php echo $p_split_primary_receiver ?>">
-    <input name="p_split_primary_receiver_fee" type="hidden" value="<?php echo $p_split_primary_receiver_fee ?>">
-    <input name="p_split_receivers[0][id]" type="hidden" value="<?php echo $p_split_receivers[0]['id'] ?>">
-    <input name="p_split_receivers[0][fee]" type="hidden" value="<?php echo $p_split_receivers[0]['fee'] ?>"> 
+    <label for="p_split_primary_receiver_fee">p_split_primary_receiver_fee</label>
+    <input name="p_split_primary_receiver_fee" type="number"></br>
     <input name="p_signature_split" type="hidden" value="<?php echo $p_signature_split ?>">
     <input type="image" id="imagen" src="https://369969691f476073508a-60bf0867add971908d4f26a64519c2aa.ssl.cf5.rackcdn.com/btns/btn1.png" />
 </form>
